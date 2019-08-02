2c9c295462de3e95c5ddd6ce28e2971b33957a9a
Bruno Vollino <bruno.vollino@ilegra.com>
2019-08-02 14:06:07 -0300

    Angular bootstrap and component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { HeaderComponent } from './header/header.component';
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    HeaderComponent,
+    AttemptsComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
